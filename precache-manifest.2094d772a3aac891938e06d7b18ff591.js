47035a4cbfe590b6c8b4680dbd4e64ef70e847fb
KiranAcharya <kiranacharya287@gmail.com>
2020-09-21 20:39:25 +0530

    Updates

---
+++ precache-manifest.2094d772a3aac891938e06d7b18ff591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283ab6fb61f56196257ca8a408792113",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "81318fab0324b70b539c",
+    "url": "/Covid19-tracker/static/css/main.eda73569.chunk.css"
+  },
+  {
+    "revision": "12cb8c14c07ee85c8837",
+    "url": "/Covid19-tracker/static/js/2.1b1a529c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid19-tracker/static/js/2.1b1a529c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81318fab0324b70b539c",
+    "url": "/Covid19-tracker/static/js/main.58717c9b.chunk.js"
+  },
+  {
+    "revision": "0bc220901b7aaad5ad25",
+    "url": "/Covid19-tracker/static/js/runtime-main.51951f26.js"
+  }
+]);
